75d462250228dd15c57d290a8cc817252f9f4006
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-04 21:05:10 -0500

    added general pseudocode for the server.js file

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 var db = require(".db/db.json");
+
+//set up Express
+
+//set up Express to handle data parsing
+
+//send user to the notes page
+
+//take user input for notes and put it into the database
+
+//delete note
+
+//start server listening
+
